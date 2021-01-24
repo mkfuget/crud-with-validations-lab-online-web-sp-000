2340b6e14a31fc082293cf5e3a72094d0055cf17
Michael Fuget <michaelfuget@gmail.com>
2021-01-24 07:32:39 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ApplicationRecord
   #validates :title, presence: true
-  #validates :released, acceptance: { accept: [true, false] }
+  validates :released, acceptance: { accept: [true, false] }
   #validate :validate_release_year
   #validate :unique_this_year?
   #validates :artist_name, presence: true
